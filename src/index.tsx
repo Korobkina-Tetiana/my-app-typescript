34e2aa62eb253e73de012dccf0a44aa45990654d
tetiana.korobkina <tetiana.korobkina@chyron.com>
2022-05-12 14:55:53 +0200

    connected files

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
+import { products } from './Products/products';
 
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <products />
   </React.StrictMode>
 );
-
-
